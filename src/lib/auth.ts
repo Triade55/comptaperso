731d03a3d8c02594b230b658f923c1c47a7758b0
Triade55 <Vangajeanulrich@gmail.com>
2024-08-03 12:04:54 +0000

    "Updated auth.ts: added interface declaration for Session, modified authOptions, and changed profile properties"

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,25 @@
 import GithubProvider from "next-auth/providers/github";
 import { env } from "./env";
-import { AuthOptions, getServerSession } from "next-auth";
+import { AuthOptions, DefaultSession, getServerSession } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "./prisma";
+declare module "next-auth" {
+  /**
+   * Returned by `auth`, `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      /** The user's postal address. */
+      id: string
+      /**
+       * By default, TypeScript merges new interface properties and overwrites existing ones.
+       * In this case, the default session user properties will be overwritten,
+       * with the new ones defined above. To keep the default session user properties,
+       * you need to add them back into the newly declared interface.
+       */
+    } & DefaultSession["user"]
+  }
+}
 export const authOptions: AuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
@@ -12,7 +29,7 @@ export const authOptions: AuthOptions = {
       clientSecret: env.GITHUB_SECRET,
       profile(profile){
         return {
-        id:profile.id.toString(),
+        id:"",
         name:profile.name,
         email : profile.email,
         username:profile.login,
